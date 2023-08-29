f42720671320373d627627184d5af4ce7d885691
Salza99 <137687037+Salza99@users.noreply.github.com>
2023-08-29 14:49:31 +0200

    update index

--- album.js
+++ album.js
@@ -1,15 +1,19 @@
-// const URL = "https://striveschool-api.herokuapp.com/api/deezer/album/75621062";
+const URL = "https://striveschool-api.herokuapp.com/api/deezer/album/81847";
+const URLSearch = "https://striveschool-api.herokuapp.com/api/deezer/search?q=linking_park";
 
-// window.addEventListener("DOMContentLoaded", async () => {
-//   const album = await fetch(URL);
-//   const res = await album.json();
-//   console.log("immaine ", res.cover);
-//   console.log(res);
-//   let cover = document.getElementsByClassName("img-fluid");
-//   cover[0].setAttribute("src", res.cover);
-//   let title = document.getElementById("title");
-//   let artist = document.getElementById("artist");
+window.addEventListener("DOMContentLoaded", async () => {
+  const search = await fetch(URLSearch);
+  const res = await search.json();
+  const album = await fetch(URL);
+  const albumRes = await album.json();
+  const realRes = res.data;
+  console.log(albumRes);
 
-//   title.innerText = res.title;
-//   artist.innerHTML = `${res.artist.name} . ${res.release_date} . ${res.nb_tracks} . ${res.duration} `;
-// });
+  let cover = document.getElementsByClassName("img-fluid");
+  cover[0].setAttribute("src", realRes[0].album.cover);
+  let title = document.getElementById("title");
+  let artist = document.getElementById("artist");
+
+  title.innerText = realRes[0].title;
+  artist.innerHTML = `${realRes[0].artist.name} . ${realRes[0].release_date} . ${realRes[0].nb_tracks} . ${realRes[0].duration} `;
+});
