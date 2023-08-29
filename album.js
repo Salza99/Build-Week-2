cc4b055745be8cc501aa1f4d45fb3a0be94affcb
Salza99 <137687037+Salza99@users.noreply.github.com>
2023-08-29 12:40:08 +0200

    album page HTML

---
+++ album.js
@@ -0,0 +1,15 @@
+// const URL = "https://striveschool-api.herokuapp.com/api/deezer/album/75621062";
+
+// window.addEventListener("DOMContentLoaded", async () => {
+//   const album = await fetch(URL);
+//   const res = await album.json();
+//   console.log("immaine ", res.cover);
+//   console.log(res);
+//   let cover = document.getElementsByClassName("img-fluid");
+//   cover[0].setAttribute("src", res.cover);
+//   let title = document.getElementById("title");
+//   let artist = document.getElementById("artist");
+
+//   title.innerText = res.title;
+//   artist.innerHTML = `${res.artist.name} . ${res.release_date} . ${res.nb_tracks} . ${res.duration} `;
+// });
