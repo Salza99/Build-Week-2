b4387e25708f93ef69b809878dff3a3232fa0e8b
Salza99 <137687037+Salza99@users.noreply.github.com>
2023-09-01 14:19:56 +0200

    Update search.js

--- search.js
+++ search.js
@@ -11,23 +11,25 @@ const search = async () => {
     for (let i = 0; i < searchResult.data.length; i++) {
       result.innerHTML += `
       <div class="col">
-          
-              
-                <a href="./index.html?albumId=${searchResult.data[i].album.id}"><div class="card text-bg-dark mb-4"><img src="${searchResult.data[i].album.cover}" alt="" class="card-img object-fit-cover" /></a>
-                  <div class="card-img-overlay overlay-dark d-flex flex-column justify-content-end">
-                <div class=""><h5 class="text-white text-truncate">${searchResult.data[i].title}</h5>
-                <a href="./index.html?artistId=${searchResult.data[i].artist.id}"><p class="text-white m-0">${searchResult.data[i].artist.name}</p></a>
+       
+        <div class="card text-bg-dark mb-4">
+         
+          <img src="${searchResult.data[i].album.cover}" alt="" class="card-img object-fit-cover" />
+            
+             <div  class="card-img-overlay overlay-dark d-flex flex-column justify-content-end">
+                <div ><h5 onclick="loadMusicOnStorage('${searchResult.data[i].title}', '${searchResult.data[i].album.cover}' ,'${searchResult.data[i].artist.name}', '${searchResult.data[i].preview}'); loadMusic();" class="text-white text-truncate">${searchResult.data[i].title}</h5>
+                  <a href="./index.html?artistId=${searchResult.data[i].artist.id}"><p class="text-white m-0">${searchResult.data[i].artist.name}</p></a>
                 </div>
-                </div>
+              </div>
                   
                 
                   
                   
-                </div>
-              
-              </div>
-          
         </div>
+              
+      </div>
+          
+        
       
       
       
